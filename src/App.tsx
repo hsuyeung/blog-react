edfc0e622eead50973cf0259b8b1fbe8449421d8
hsuyeung <hsuyeung.com@gmail.com>
2022-04-30 14:03:26 +0800

    feat: a demo

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App(props: any) {
+  return (
+    <div className='App'>
+      <p>I am {props.name}</p>
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,5 @@
-import React  from "react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
+ReactDOM.render(<App name='Hsu Yeung' />, document.getElementById('root'));
